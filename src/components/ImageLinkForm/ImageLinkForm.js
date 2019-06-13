192d6447e6f063a262471ebc89c745627401f44e
Michael Williams <bsdfool@gmail.com>
2019-06-13 18:22:22 -0400

    add ImageLinkForm js and css files

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return (
+        <div className="ma4 mt0">
+            <p className="f3">
+                {'This magic brain will detect faces in your pictures. Give it a try...'}
+            </p>
+            <div className="center">
+                <div className="form center pa4 br3 shadow-4">
+                    <input className="f4 pa2 w-70 center" type="text" />
+                    <button className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple">Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div>
+        <div className="ma4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa3"><img className="pt1" src={brain} alt='logo' /></div>
             </Tilt>
